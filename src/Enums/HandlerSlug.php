3e3c517d506b8fd0a11f6af31e368703cf371e42
Kiko Seijo (mmac) <kiko@sunnyface.com>
2026-03-26 03:32:26 +0100

    feat: add CustomsAdvisor case to HandlerSlug enum and update related methods

--- src/Enums/HandlerSlug.php
+++ src/Enums/HandlerSlug.php
@@ -10,6 +10,7 @@ enum HandlerSlug: string
 {
     case Talker = 'talker';
     case FinancialAdvisor = 'financial-advisor';
+    case CustomsAdvisor = 'customs-advisor';
     case TextTranslator = 'text-translator';
     case VisionExtractor = 'vision-extractor';
     case DocumentClassifier = 'document-classifier';
@@ -21,6 +22,7 @@ enum HandlerSlug: string
         return match ($this) {
             self::Talker => 'Conversacional (Talker)',
             self::FinancialAdvisor => 'Asesor Financiero (Agentic)',
+            self::CustomsAdvisor => 'Asesor de Aduanas (Agentic)',
             self::TextTranslator => 'Traductor de Texto',
             self::VisionExtractor => 'Extractor Visual',
             self::DocumentClassifier => 'Clasificador de Documentos',
@@ -37,7 +39,7 @@ enum HandlerSlug: string
     public function uiManifest(): UiManifestData
     {
         return match ($this) {
-            self::Talker, self::FinancialAdvisor, self::MetaAgent, self::TextTranslator => new UiManifestData(
+            self::Talker, self::CustomsAdvisor, self::FinancialAdvisor, self::MetaAgent, self::TextTranslator => new UiManifestData(
                 components: [AgentComponent::Chat, AgentComponent::BitacoraWidget],
             ),
             self::VisionExtractor, self::DocumentClassifier => new UiManifestData(
